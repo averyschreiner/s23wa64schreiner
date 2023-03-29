b3bd1bc669891a955f201f952014c5247072fd08
Avery Schreiner <schreineravery@gmail.com>
2023-03-29 14:04:12 -0500

    part 5

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let sum = more = 0
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more += 1
+  sum += more
+  res.send(`Sum = ${sum}`);
 });
 
 module.exports = router;
